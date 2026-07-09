cf7989509ea3cc0b0ed37fb3e146574f5b595b67
JanhviVatsya <janhvivatsya24@gmail.com>
2026-07-09 21:18:13 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/recursionvisualizer/RecursionVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.recursionvisualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursionVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursionVisualizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recursionvisualizer/RecursionVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recursionvisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecursionVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
